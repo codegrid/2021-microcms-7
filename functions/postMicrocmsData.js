914c8571d7f147d9456ca950d50a73667a25802c
yuto <namawasabi1229@gmail.com>
2021-06-17 17:07:39 +0900

    first commit

---
+++ functions/postMicrocmsData.js
@@ -0,0 +1,18 @@
+exports.handler = async (event, context) => {
+	
+  const requestUrl = Process.env.CMS_POST_URL;
+  const dataResponse = await fetch(requestUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-WRITE-API-KEY": process.env.X_WRITE_API_KEY,
+    },
+    body: JSON.stringify({ body: "hello from netlify functions" }),
+  });
+  const data = await dataResponse.json();
+
+	return {
+		statusCode: 200,
+		body: data
+	};
+}
