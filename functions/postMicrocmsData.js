30c084840b5833b397334713bacaf199f2eee840
yuto <namawasabi1229@gmail.com>
2021-06-17 18:13:48 +0900

    use querystring

--- functions/postMicrocmsData.js
+++ functions/postMicrocmsData.js
@@ -1,19 +1,21 @@
 const fetch = require('node-fetch');
+const querystring = require("querystring");
 
 exports.handler = async (event, context) => {
   if (event.httpMethod !== "POST") {
     return  { statusCode: 405, body: "Method Not Allowed" };
   }
-  console.log(event.body.content)
-  const requestUrl = process.env.CMS_POST_URL;
 
+  const params = querystring.parse(event.body)
+
+  const requestUrl = process.env.CMS_POST_URL;
   const dataResponse = await fetch(requestUrl, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "X-WRITE-API-KEY": process.env.X_WRITE_API_KEY,
     },
-    body: JSON.stringify({ content: event.body.content }),
+    body: JSON.stringify({ content: params.content }),
   });
   const responceData = await dataResponse.json();
 
