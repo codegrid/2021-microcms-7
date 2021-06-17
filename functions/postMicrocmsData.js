30cf9aecac7a710f40a90ab3f092d708fa0792d1
yuto <namawasabi1229@gmail.com>
2021-06-17 18:24:53 +0900

    update

--- functions/postMicrocmsData.js
+++ functions/postMicrocmsData.js
@@ -1,16 +1,12 @@
 const fetch = require('node-fetch');
-const querystring = require("querystring");
 
 exports.handler = async (event, context) => {
   if (event.httpMethod !== "POST") {
     return  { statusCode: 405, body: "Method Not Allowed" };
   }
+  const requestBody = JSON.parse(event.body)
 
-  console.log(event.body.json())
-  const params = event.body.json()
-  console.log(params)
-  console.log(params.content)
-  console.log(event.queryStringParameters.content)
+  console.log(requestBody)
   const requestUrl = process.env.CMS_POST_URL;
   const dataResponse = await fetch(requestUrl, {
     method: "POST",
@@ -18,7 +14,7 @@ exports.handler = async (event, context) => {
       "Content-Type": "application/json",
       "X-WRITE-API-KEY": process.env.X_WRITE_API_KEY,
     },
-    body: JSON.stringify({ content: params.content }),
+    body: JSON.stringify({ content: requestBody.content }),
   });
   const responceData = await dataResponse.json();
 
