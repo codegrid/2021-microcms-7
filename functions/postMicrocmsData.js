1f841dc5ff7673cdb734c83733ccc27d5bf3d7aa
yuto <namawasabi1229@gmail.com>
2021-06-17 17:34:16 +0900

    user node-fetch defalut

--- functions/postMicrocmsData.js
+++ functions/postMicrocmsData.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch')
+const fetch = require('node-fetch').default;
 exports.handler = async (event, context) => {
   const requestUrl = process.env.CMS_POST_URL;
   const dataResponse = await fetch(requestUrl, {
